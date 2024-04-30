deb5d5f660e4922c7d80d2f79b64d8b9fe3e01ad
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-30 18:39:36 +0200

    Create base book services and its configs

---
+++ src/services/apis/book/book.services.ts
@@ -0,0 +1,25 @@
+import config from './config';
+
+const { BASE_BOOKS_API_URL } = config;
+
+async function getBooks() {
+  try {
+    const response = await fetch(`${BASE_BOOKS_API_URL}/books`);
+    const books = await response.json();
+    return books;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getBookById(id: number) {
+  try {
+    const response = await fetch(`${BASE_BOOKS_API_URL}/books/${id}`);
+    const book = await response.json();
+    return book;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { getBooks, getBookById };
---
+++ src/services/apis/book/config.ts
@@ -0,0 +1,3 @@
+export default {
+  BASE_BOOKS_API_URL: import.meta.env.VITE_BASE_BOOKS_API_URL,
+};
