0f623e2ca016fe99300d6098257c705b2910261e
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-30 19:32:20 +0200

    Add additional path alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +11,13 @@ export default defineConfig({
       { find: '@assets', replacement: resolve(__dirname, './src/assets') },
       { find: '@components', replacement: resolve(__dirname, './src/components') },
       { find: '@context', replacement: resolve(__dirname, './src/context') },
+      { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
       { find: '@pages', replacement: resolve(__dirname, './src/pages') },
       { find: '@routes', replacement: resolve(__dirname, './src/routes') },
+      { find: '@services', replacement: resolve(__dirname, './src/services') },
       { find: '@styles', replacement: resolve(__dirname, './src/styles') },
+      { find: '@app-types', replacement: resolve(__dirname, './src/types') },
       { find: '@utils', replacement: resolve(__dirname, './src/utils') },
     ],
   },
-})
+});
