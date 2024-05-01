66ceaf97a949a0207052723fc8737ea481d8fd84
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-05-01 05:04:53 +0200

    Add description and genre fields to Book entity

--- src/app/domain/Book.ts
+++ src/app/domain/Book.ts
@@ -1,17 +1,33 @@
 import BaseEntity from './BaseEntity';
 
+interface GenreCollection<T> {
+  length: 3;
+  [index: number]: T;
+}
 export default class Book extends BaseEntity {
   title?: string = '';
   author?: string = '';
   price?: number = 0;
   coverUrl?: string = '';
+  genres?: GenreCollection<string>;
+  description?: string = '';
 
-  constructor(id: number, title: string, author: string, price: number, cover_url?: string) {
+  constructor(
+    id: number,
+    title: string,
+    author: string,
+    price: number,
+    cover_url?: string,
+    genres?: GenreCollection<string>,
+    description?: string,
+  ) {
     super(id);
 
     this.title = title;
     this.author = author;
     this.price = price;
     this.coverUrl = cover_url;
+    this.genres = genres;
+    this.description = description;
   }
 }
