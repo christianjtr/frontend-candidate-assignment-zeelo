0891bc2808123dc73675ee40125af20514fc5560
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-05-02 02:55:39 +0200

    Include mocked error implementation into useFetchHook

--- src/hooks/useFetchBooks.ts
+++ src/hooks/useFetchBooks.ts
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
+import { toast } from 'sonner';
 import { getBooks } from '@services/apis/book/book.services';
+import type { PaginatedType } from '@app-types/Paginated';
+import type { CustomError } from '@utils/errors';
 import Book from '@app/domain/Book';
 
 export interface UseFetchBooksInterface {
@@ -8,7 +11,7 @@ export interface UseFetchBooksInterface {
   hasError: boolean;
 }
 
-export const useFetchBooks = (): UseFetchBooksInterface => {
+export const useFetchBooks = (paginated?: PaginatedType): UseFetchBooksInterface => {
   const [books, setBooks] = useState<Book[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [hasError, setHasError] = useState<boolean>(false);
@@ -17,14 +20,19 @@ export const useFetchBooks = (): UseFetchBooksInterface => {
     setIsLoading(true);
 
     try {
-      const response = await getBooks();
+      const response = await getBooks(paginated);
       const responseDTO = response.map(
         (item) => new Book(item.id, item.title, item.author, item.price, item.cover_url),
       );
       setBooks(responseDTO);
     } catch (error) {
+      const {
+        error: { code, message },
+      } = error as { error: CustomError };
+
       setHasError(true);
-      throw new Error(error as string);
+      toast(`[Error - ${code}] : ${message}`, { className: 'toaster toaster--error' });
+      throw new Error(message);
     } finally {
       setIsLoading(false);
     }
