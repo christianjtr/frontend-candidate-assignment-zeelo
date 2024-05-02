458a1ef537af2178b8d3c3baca09031cec5099ff
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-05-02 22:19:10 +0200

    Add mocked purchase data

--- src/app/application/api/db/index.cjs
+++ src/app/application/api/db/index.cjs
@@ -1,7 +1,9 @@
 const books = require('./books.json');
 const genres = require('./genres.json');
+const purchases = require('./purchases.json');
 
 module.exports = () => ({
   books,
   genres,
+  purchases,
 });
